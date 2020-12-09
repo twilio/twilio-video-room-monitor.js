9eb4c41766531f2a0fd26e3768dfca8525ecd5e6
Tim Mendoza <tmendoza@twilio.com>
2020-12-08 22:35:34 -0700

    Add useParticipants hook

---
+++ src/hooks/useParticipants/useParticipants.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { RemoteParticipant } from 'twilio-video';
+import useRoom from '../useRoom/useRoom';
+
+export default function useParticipants() {
+  const room = useRoom();
+  const [participants, setParticipants] = useState(room ? Array.from(room.participants.values()) : []);
+
+  useEffect(() => {
+    if (!room) return;
+
+    const participantConnected = (participant: RemoteParticipant) =>
+      setParticipants((prevParticipants) => [...prevParticipants, participant]);
+    const participantDisconnected = (participant: RemoteParticipant) =>
+      setParticipants((prevParticipants) => prevParticipants.filter((p) => p !== participant));
+
+    room.on('participantConnected', participantConnected);
+    room.on('participantDisconnected', participantDisconnected);
+    return () => {
+      room.off('participantConnected', participantConnected);
+      room.off('participantDisconnected', participantDisconnected);
+    };
+  }, [room]);
+
+  return participants;
+}
