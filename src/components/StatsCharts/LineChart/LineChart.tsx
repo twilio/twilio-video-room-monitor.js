936883479b981cff53a05fd534962ffb0856c1f4
Tim Mendoza <tmendoza@twilio.com>
2021-05-17 22:32:22 -0600

    Update styling on line chart

--- src/components/StatsCharts/LineChart/LineChart.tsx
+++ src/components/StatsCharts/LineChart/LineChart.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
-import { min, max } from 'd3-array';
-import { MAX_STAT_HISTORY_LENGTH, UPDATE_INTERVAL } from '../../../constants';
-import { XYChart, Axis, LineSeries, Grid } from '@visx/xychart';
+import { chartDatum } from '../../../types';
 import { connectNulls } from '../../../utils/utils';
-import { datum } from '../../../types';
-import styled from 'styled-components';
+import Headline from '../../typography/Headline/Headline';
+import { MAX_STAT_HISTORY_LENGTH, UPDATE_INTERVAL } from '../../../constants';
+import { min, max } from 'd3-array';
+import { XYChart, Axis, LineSeries, Grid, buildChartTheme } from '@visx/xychart';
 
-const ChartContainer = styled.div`
-  margin: 10px;
-  height: 200px;
-`;
+export function formatBitrate(bytes: number, suffixIndex = 0): string {
+  const suffixes = ['kbps', 'mbps', 'gbps'];
+  if (bytes < 1000) return +bytes.toFixed(0) + ' ' + suffixes[suffixIndex];
+  return formatBitrate(bytes / 1024, suffixIndex + 1);
+}
 
-const Title = styled.h5``;
+const theme = buildChartTheme({
+  backgroundColor: 'transparent',
+  colors: ['#E22525'],
+  tickLength: 2,
+  gridColor: '#333',
+  gridColorDark: '#333',
+  svgLabelSmall: { strokeWidth: 0.2, stroke: '#ddd', fill: '#ddd' },
+  svgLabelBig: { strokeWidth: 0, fill: '#ddd' },
+});
 
 interface LineChartProps {
-  data: datum[];
+  data: chartDatum[];
   title: string;
   yAxisLabel: string;
   hidden: boolean;
@@ -29,11 +38,11 @@ export default function LineChart({ data, title, yAxisLabel, hidden }: LineChart
   if (hidden) return null;
 
   return (
-    <ChartContainer>
-      <Title>{title}</Title>
+    <>
+      <Headline>{title}</Headline>
       <XYChart
-        height={250}
-        width={450}
+        height={275}
+        width={465}
         xScale={{
           type: 'time',
           domain: [xDomainMin, max(filteredData, (d) => d.x) ?? 0],
@@ -42,13 +51,21 @@ export default function LineChart({ data, title, yAxisLabel, hidden }: LineChart
           type: 'linear',
           domain: [0, Math.max(50, max(data, (d) => d.y)!)],
         }}
+        margin={{ top: 10, left: 75, right: 0, bottom: 50 }}
+        theme={theme}
       >
         <Grid columns={false} numTicks={5} strokeDasharray="2" />
         <Grid rows={false} strokeDasharray="2" />
         <Axis orientation="bottom" label="Time" />
-        <Axis orientation="left" label={yAxisLabel} numTicks={5} />
+        <Axis
+          orientation="left"
+          label={yAxisLabel}
+          numTicks={5}
+          tickFormat={(d) => formatBitrate(d)}
+          labelOffset={48}
+        />
         <LineSeries dataKey="x" data={filteredData} xAccessor={(d) => d.x} yAccessor={(d) => d.y} />
       </XYChart>
-    </ChartContainer>
+    </>
   );
 }
