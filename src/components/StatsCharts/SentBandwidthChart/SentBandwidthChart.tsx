8cce2f4ea8005406fdd5125dae7621fd388d3744
Tim Mendoza <tmendoza@twilio.com>
2021-05-17 21:30:24 -0600

    Update sent and received bandwidth charts

--- src/components/StatsCharts/SentBandwidthChart/SentBandwidthChart.tsx
+++ src/components/StatsCharts/SentBandwidthChart/SentBandwidthChart.tsx
@@ -1,90 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import ReactApexChart from 'react-apexcharts';
-import styled from 'styled-components';
-import { useTotalBandwidth } from '../../../hooks/useStats/useStats';
+import React from 'react';
+import useStats from '../../../hooks/useStats/useStats';
+import LineChart from '../LineChart/LineChart';
 
-const ChartContainer = styled.div`
-  margin: 10px;
-  height: 200px;
-`;
-
-const apexOptions = {
-  chart: {
-    background: 'transparent',
-    id: 'sentBandwidth',
-    type: 'line',
-    animations: {
-      enabled: false,
-    },
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-  },
-  colors: ['#f00'],
-  theme: {
-    mode: 'dark',
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: 'smooth',
-  },
-  title: {
-    text: 'Total Bytes Sent (kbps)',
-    align: 'left',
-  },
-  markers: {
-    size: 0,
-  },
-  xaxis: {
-    range: 75,
-    axisTicks: {
-      show: false,
-    },
-    tooltip: {
-      enabled: false,
-    },
-    labels: {
-      show: false,
-    },
-  },
-  yaxis: { min: 0, max: 1000 },
-  legend: {
-    show: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-};
-
-export default function SentBandwidthChart() {
-  const totalBandwidth = useTotalBandwidth('bytesSent');
-  const [bandwidthArr, setBandwidthArr] = useState<{ x: number; y: number }[]>([]);
-
-  useEffect(() => {
-    setBandwidthArr(function (prev) {
-      let y = 0;
-      if (totalBandwidth === null || totalBandwidth < 0) {
-        y = prev[prev.length - 1]?.y || 0;
-      } else {
-        y = totalBandwidth;
-      }
-      return [...prev, { x: Date.now() / 1000, y: y }];
-    });
-  }, [totalBandwidth]);
+export default function SentBandwidthChart({ hidden }: { hidden: boolean }) {
+  const { sentBytesHistory } = useStats();
 
   return (
-    <ChartContainer>
-      <ReactApexChart
-        options={apexOptions}
-        series={[{ name: 'Bytes Sent (kbps)', data: bandwidthArr }]}
-        type="line"
-        height="100%"
-      />
-    </ChartContainer>
+    <LineChart data={sentBytesHistory} yAxisLabel={'Bytes Sent'} hidden={hidden} title="Total Bytes Sent (kbps)" />
   );
 }
