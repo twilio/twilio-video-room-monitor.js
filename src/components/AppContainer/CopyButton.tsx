3c25c6bb7fdbdbc7aa5b84450b2c7e9da9abcbf7
Tim Mendoza <tmendoza@twilio.com>
2021-05-17 21:28:02 -0600

    Fix SVG attributes

--- src/components/AppContainer/CopyButton.tsx
+++ src/components/AppContainer/CopyButton.tsx
@@ -27,14 +27,14 @@ export function CopyButton() {
     <CopyButtonContainer onClick={handleRoomCopy} hasRoom={!!room} title="Copy Room Information">
       <svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M4.917.413c0-.274.223-.496.5-.496h6.666c.133 0 .26.052.354.145l5 4.959c.093.093.146.219.146.35v11.57a.498.498 0 01-.5.496H5.417a.498.498 0 01-.5-.495V.412zm1 .495v15.538h10.666V5.576L11.876.909h-5.96z"
           fill="#DDD"
         />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M12.083-.083c.276 0 .5.222.5.496v4.462h4.5c.276 0 .5.222.5.496a.498.498 0 01-.5.496h-5a.498.498 0 01-.5-.496V.413c0-.274.224-.496.5-.496zM2.417 2.892c0-.274.223-.496.5-.496h2.5c.276 0 .5.222.5.496a.498.498 0 01-.5.496h-2v15.537h10.666v-1.983c0-.274.224-.496.5-.496s.5.222.5.495v2.48a.498.498 0 01-.5.496H2.917a.498.498 0 01-.5-.496V2.89z"
           fill="#DDD"
         />
