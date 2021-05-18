47a080bf852218f71a59c9e22ee334d7547b052e
Tim Mendoza <tmendoza@twilio.com>
2021-05-17 21:27:30 -0600

    Create utils file

---
+++ src/utils/utils.ts
@@ -0,0 +1,15 @@
+import { datum } from '../types';
+
+export function connectNulls(data: datum[]) {
+  return data.map((d, i) => {
+    const previousY = data[i - 1]?.y;
+    if (d.y === null && data[i - 1]?.y !== null) {
+      return { x: d.x, y: previousY };
+    } else {
+      return d;
+    }
+  });
+}
+
+export const truncateFront = (arr: any[], limit: number) =>
+  arr.slice(Math.max(0, arr.length - limit), arr.length + limit);
