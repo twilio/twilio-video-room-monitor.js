ed97b4124551a52fa3ed5c59f91718470f253cdc
Tim Mendoza <tmendoza@twilio.com>
2021-05-18 14:43:59 -0600

    Update tests for RoomStatsProvider

--- src/components/RoomStatsProvider/RoomStatsProvider.test.tsx
+++ src/components/RoomStatsProvider/RoomStatsProvider.test.tsx
@@ -3,7 +3,11 @@ import { renderHook } from '@testing-library/react-hooks';
 import RoomStatsProvider from './RoomStatsProvider';
 import useGetStats from '../../hooks/useGetStats/useGetStats';
 import useRoom from '../../hooks/useRoom/useRoom';
-import useStats from '../../hooks/useStats/useStats';
+import * as statsHooks from '../../hooks/useStats/useStats';
+
+// @ts-ignore
+statsHooks.getTotalBandwidth = jest.fn((kind) => (kind === 'bytesReceived' ? 0 : 1));
+const mockGetTotalBandwidth = statsHooks.getTotalBandwidth as jest.Mock<any>;
 
 jest.mock('../../hooks/useRoom/useRoom', () => jest.fn(() => 'mockRoom'));
 jest.mock('../../hooks/useGetStats/useGetStats', () => jest.fn(() => 'mockStats'));
@@ -14,16 +18,49 @@ const mockUseRoom = useRoom as jest.Mock<any>;
 describe('the RoomStatsProvider component', () => {
   it('should correctly return stats and previousStats', () => {
     const wrapper: React.FC = ({ children }) => <RoomStatsProvider>{children}</RoomStatsProvider>;
-    const { result, rerender } = renderHook(useStats, { wrapper });
+    const { result, rerender } = renderHook(statsHooks.useStats, { wrapper });
 
     expect(mockUseRoom).toHaveBeenCalled();
     expect(mockUseGetStats).toHaveBeenCalledWith('mockRoom');
 
-    expect(result.current).toEqual({ previousStats: undefined, stats: 'mockStats' });
+    expect(result.current).toMatchObject({ previousStats: undefined, stats: 'mockStats' });
 
     mockUseGetStats.mockImplementationOnce(() => 'mockStats2');
     rerender();
 
-    expect(result.current).toEqual({ previousStats: 'mockStats', stats: 'mockStats2' });
+    expect(result.current).toMatchObject({ previousStats: 'mockStats', stats: 'mockStats2' });
+  });
+
+  it('should correctly return bandwidth statistics and history', () => {
+    Date.now = () => 1000;
+
+    const wrapper: React.FC = ({ children }) => <RoomStatsProvider>{children}</RoomStatsProvider>;
+    const { result, rerender } = renderHook(statsHooks.useStats, { wrapper });
+
+    expect(result.current).toMatchObject({
+      receivedBitrateHistory: [{ x: 1000, y: 0 }],
+      sentBitrateHistory: [{ x: 1000, y: 1 }],
+      currentReceivedBitrate: 0,
+      currentSentBitrate: 1,
+    });
+
+    Date.now = () => 2000;
+    mockGetTotalBandwidth.mockImplementation((kind) => (kind === 'bytesReceived' ? 2 : 3));
+
+    mockUseGetStats.mockImplementationOnce(() => 'mockStats2');
+    rerender();
+
+    expect(result.current).toMatchObject({
+      receivedBitrateHistory: [
+        { x: 1000, y: 0 },
+        { x: 2000, y: 2 },
+      ],
+      sentBitrateHistory: [
+        { x: 1000, y: 1 },
+        { x: 2000, y: 3 },
+      ],
+      currentReceivedBitrate: 2,
+      currentSentBitrate: 3,
+    });
   });
 });
