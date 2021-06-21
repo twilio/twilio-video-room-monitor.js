0535061769102aca2fef386be0e4aa65e49daee6
Vivian Bai <vbai@twilio.com>
2021-06-21 16:07:55 -0700

    updated datum to use type Primitive for value

--- src/components/typography/Datum/Datum.tsx
+++ src/components/typography/Datum/Datum.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Primitive } from '../../../types';
 import { theme } from '../../theme';
 
 type DatumProps = {
   label: string;
-  value: string | number | boolean | null | undefined;
+  value: Primitive;
 };
 
 const Text = styled.span`
