5f0ee00d652b9a8e71a3d4b864a33594c2644076
Vivian Bai <vbai@twilio.com>
2021-06-21 09:07:31 -0700

    display information for connection options

--- src/components/RoomInfo/RoomInfo.tsx
+++ src/components/RoomInfo/RoomInfo.tsx
@@ -6,6 +6,9 @@ import useIsRecording from '../../hooks/useIsRecording/useIsRecording';
 import useRoom from '../../hooks/useRoom/useRoom';
 import useRoomState from '../../hooks/useRoomState/useRoomState';
 import useStats from '../../hooks/useStats/useStats';
+import { Accordion } from '../typography/Accordion/Accordion';
+import NestedObject from '../typography/NestedObject/NestedObject';
+import ArrayData from '../typography/ArrayData/ArrayData';
 
 export default function RoomInfo() {
   const room = useRoom();
@@ -15,6 +18,29 @@ export default function RoomInfo() {
   const { currentReceivedBitrate, currentSentBitrate } = useStats();
 
   if (!room) return null;
+
+  const {
+    audio,
+    automaticSubscription,
+    bandwidthProfile,
+    dscpTagging,
+    enableDscp,
+    eventListener,
+    iceServers,
+    iceTransportPolicy,
+    insights,
+    maxAudioBitrate,
+    maxVideoBitrate,
+    name,
+    networkQuality,
+    region,
+    preferredAudioCodecs,
+    preferredVideoCodecs,
+    logLevel,
+    loggerName,
+    tracks,
+    video,
+  }: any = room._options;
 
   return (
     <div style={{ marginBottom: '1em' }}>
@@ -28,9 +54,33 @@ export default function RoomInfo() {
           <Datum label="State" value={roomState} />
           <Datum label="Dominant Speaker" value={String(dominantSpeaker?.identity || null)} />
           <Datum label="Media Region" value={room.mediaRegion} />
-          <Datum label="Is Recording" value={String(isRecording)} />
-          <Datum label="Total Sent Bandwidth" value={String(currentSentBitrate?.toLocaleString()) + 'kbps'} />
-          <Datum label="Total Received Bandwidth" value={String(currentReceivedBitrate?.toLocaleString()) + 'kbps'} />
+          <Datum label="Is Recording" value={isRecording} />
+          <Datum label="Total Sent Bandwidth" value={currentSentBitrate?.toLocaleString() + 'kbps'} />
+          <Datum label="Total Received Bandwidth" value={currentReceivedBitrate?.toLocaleString() + 'kbps'} />
+
+          <Accordion label="Connection Options">
+            <NestedObject label="Audio" obj={audio} />
+            <Datum label="Automatic Subscription" value={automaticSubscription} />
+            <NestedObject label="Bandwidth Profile" obj={bandwidthProfile} />
+            <Datum label="Dominant Speaker" value={Boolean(dominantSpeaker)} />
+            <Datum label="DSCP Tagging" value={dscpTagging} />
+            <Datum label="Enable DSCP" value={enableDscp} />
+            <NestedObject label="Event Listener" obj={eventListener} />
+            <ArrayData label="Ice Servers" arr={iceServers} />
+            <NestedObject label="Ice Transport Policy" obj={iceTransportPolicy} />
+            <Datum label="Insights" value={insights} />
+            <Datum label="Max Audio Bitrate" value={maxAudioBitrate} />
+            <Datum label="Max Video Bitrate" value={maxVideoBitrate} />
+            <Datum label="Name" value={name} />
+            <NestedObject label="Network Quality" obj={networkQuality} />
+            <Datum label="Region" value={region} />
+            <ArrayData label="Preferred Audio Codecs" arr={preferredAudioCodecs} />
+            <ArrayData label="Preferred Video Codecs" arr={preferredVideoCodecs} />
+            <NestedObject label="Log Level" obj={logLevel} />
+            <Datum label="Logger Name" value={loggerName} />
+            <ArrayData label="Tracks" arr={tracks} />
+            <NestedObject label="Video" obj={video} />
+          </Accordion>
         </>
       )}
     </div>
