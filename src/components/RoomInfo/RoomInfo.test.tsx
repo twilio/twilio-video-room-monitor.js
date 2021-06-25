86110a2620f433e1f5cedec16111147b7944410b
Vivian Bai <vbai@twilio.com>
2021-06-25 11:58:03 -0700

    simplified roominfo test and updated snapshot

--- src/components/RoomInfo/RoomInfo.test.tsx
+++ src/components/RoomInfo/RoomInfo.test.tsx
@@ -2,82 +2,13 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import RoomInfo from './RoomInfo';
 import useRoomState from '../../hooks/useRoomState/useRoomState';
+import useRoom from '../../hooks/useRoom/useRoom';
 
 jest.mock('../../hooks/useDominantSpeaker/useDominantSpeaker', () => () => ({
   dominantSpeaker: { identity: 'testIdentity' },
 }));
 jest.mock('../../hooks/useIsRecording/useIsRecording', () => () => 'false');
-jest.mock('../../hooks/useRoom/useRoom', () => () => ({
-  name: 'test123',
-  sid: 'XXXXXXXXXXXXX1234',
-  mediaRegion: 'testRegion',
-  _options: {
-    audio: { logLevel: {}, loggerName: 'twilio-audio', name: 'testName', workaroundWebKitBug180748: true },
-    automaticSubscription: true,
-    bandwidthProfile: {
-      video: {
-        dominantSpeakerPriority: 'standard',
-        maxSubscriptionBitrate: 10,
-        clientTrackSwitchOffControl: 'auto',
-        contentPreferencesMode: 'auto',
-        maxTracks: 100,
-        mode: 'grid',
-        renderDimensions: {
-          high: { width: 1080, height: 720 },
-          low: { width: 100, height: 70 },
-          standard: { width: 340, height: 190 },
-        },
-        trackSwitchOffMode: 'predicted',
-      },
-    },
-    dominantSpeaker: true,
-    dscpTagging: true,
-    enableDscp: false,
-    iceServers: [],
-    iceTransportPolicy: undefined,
-    insights: true,
-    maxAudioBitrate: 1600,
-    maxVideoBitrate: null,
-    name: 'testRoom',
-    networkQuality: {
-      local: 1,
-      remote: 1,
-    },
-    region: 'testRegion',
-    preferredAudioCodecs: [
-      {
-        isa: 'testIsa',
-        opus: 'testOpus',
-        PCMA: 'testPCMA',
-        PCMU: 'testPCMU',
-      },
-    ],
-    preferredVideoCodecs: [
-      {
-        codec: 'VP0',
-        simulcast: false,
-      },
-      {
-        codec: 'VP8',
-        simulcast: true,
-      },
-    ],
-    loggerName: 'logger-twilio-video',
-    tracks: [
-      {
-        isStopped: false,
-        isEnabled: false,
-        id: '32e1f5ed-1b4b-470e-bc52-03399b5d847b',
-        processedTrack: null,
-        mediaStreamTrack: {},
-        isStarted: true,
-        name: '32e1f5ed-1b4b-470e-bc52-03399b5d847b',
-        kind: 'audio',
-      },
-    ],
-    video: { logLevel: {}, loggerName: 'twilio-video', name: 'testName', workaroundWebKitBug180748: false },
-  },
-}));
+jest.mock('../../hooks/useRoom/useRoom');
 
 jest.mock('../../hooks/useRoomState/useRoomState');
 jest.mock('../../hooks/useStats/useStats', () => () => ({
@@ -88,10 +19,47 @@ jest.mock('../../hooks/useStats/useStats', () => () => ({
 const mockUseRoomState = useRoomState as jest.Mock<any>;
 mockUseRoomState.mockImplementation(() => 'connected');
 
+const mockUseRoom = useRoom as jest.Mock<any>;
+
+beforeEach(() => {
+  mockUseRoom.mockImplementation(() => ({
+    name: 'test123',
+    sid: 'XXXXXXXXXXXXX1234',
+    mediaRegion: 'testRegion',
+    _options: {
+      audio: true,
+      automaticSubscription: true,
+      bandwidthProfile: 'mockBandwidthProfile',
+      dominantSpeaker: true,
+      dscpTagging: true,
+      enableDscp: false,
+      iceServers: [],
+      iceTransportPolicy: undefined,
+      insights: true,
+      maxAudioBitrate: 1600,
+      maxVideoBitrate: null,
+      name: 'testRoom',
+      networkQuality: true,
+      region: 'testRegion',
+      preferredAudioCodecs: ['PCMU'],
+      preferredVideoCodecs: ['VP8'],
+      loggerName: 'logger-twilio-video',
+      tracks: ['mockTrack'],
+      video: true,
+    },
+  }));
+});
+
 describe('The RoomInfo component', () => {
   it('should render information about the room if connected to a Twilio Video room', () => {
     const wrapper = shallow(<RoomInfo />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should not render anything and return null if the room does not exist', () => {
+    mockUseRoom.mockImplementationOnce(() => null);
+    const wrapper = shallow(<RoomInfo />);
+    expect(wrapper.getElement()).toBe(null);
   });
 
   it('should display warning when not connected to a Twilio Video room', () => {
