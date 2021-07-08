bd10be7ee0ae8e75eaffc4652d5c24521f4c3e74
vwbai <40278237+vwbai@users.noreply.github.com>
2021-07-08 14:12:23 -0400

    Renaming video inspector to room monitor (#17)
    * renaming video inspector to room monitor
    
    * updated the naming of the RoomMonitor
    
    Co-authored-by: Vivian Bai <vbai@twilio.com>

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,73 +1,73 @@
 import React from 'react';
 import { roomRegistry } from './components/RoomProvider/RoomProvider';
-import TwilioVideoInspector from './index';
+import RoomMonitor from './index';
 
 jest.mock('./App', () => () => <div>Test</div>);
 
-describe('the Twilio Video Inspector API', () => {
-  afterEach(() => TwilioVideoInspector.closeInspector());
+describe('the Twilio Video Room Monitor API', () => {
+  afterEach(() => RoomMonitor.closeMonitor());
 
   it('should render the app to the document when opened, and remove it when closed', () => {
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
 
-    TwilioVideoInspector.openInspector();
+    RoomMonitor.openMonitor();
     expect(document.querySelector('div')?.textContent).toEqual('Test');
 
-    TwilioVideoInspector.closeInspector();
+    RoomMonitor.closeMonitor();
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
   });
 
   it('should not render the app when it is already open', () => {
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
 
-    TwilioVideoInspector.openInspector();
-    TwilioVideoInspector.openInspector();
+    RoomMonitor.openMonitor();
+    RoomMonitor.openMonitor();
 
-    expect(document.querySelectorAll('#TwilioVideoInspectorContainer').length).toEqual(1);
+    expect(document.querySelectorAll('#RoomMonitorContainer').length).toEqual(1);
   });
 
   it('should not close the app when it is already closed', () => {
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
 
-    TwilioVideoInspector.closeInspector();
+    RoomMonitor.closeMonitor();
 
-    expect(document.querySelectorAll('#TwilioVideoInspectorContainer').length).toEqual(0);
+    expect(document.querySelectorAll('#RoomMonitorContainer').length).toEqual(0);
   });
 
   it('should emit events when opened and closed', () => {
-    jest.spyOn(TwilioVideoInspector, 'emit');
+    jest.spyOn(RoomMonitor, 'emit');
 
-    TwilioVideoInspector.openInspector();
-    expect(TwilioVideoInspector.emit).toHaveBeenCalledWith('opened');
+    RoomMonitor.openMonitor();
+    expect(RoomMonitor.emit).toHaveBeenCalledWith('opened');
 
-    TwilioVideoInspector.closeInspector();
-    expect(TwilioVideoInspector.emit).toHaveBeenCalledWith('closed');
+    RoomMonitor.closeMonitor();
+    expect(RoomMonitor.emit).toHaveBeenCalledWith('closed');
   });
 
-  it('should correctly set isOpen when the inspector is opened or closed', () => {
-    jest.spyOn(TwilioVideoInspector, 'emit');
+  it('should correctly set isOpen when the monitor is opened or closed', () => {
+    jest.spyOn(RoomMonitor, 'emit');
 
-    TwilioVideoInspector.openInspector();
-    expect(TwilioVideoInspector.isOpen).toBe(true);
+    RoomMonitor.openMonitor();
+    expect(RoomMonitor.isOpen).toBe(true);
 
-    TwilioVideoInspector.closeInspector();
-    expect(TwilioVideoInspector.isOpen).toBe(false);
+    RoomMonitor.closeMonitor();
+    expect(RoomMonitor.isOpen).toBe(false);
   });
 
   it('should emit events when toggled opened and closed', () => {
-    jest.spyOn(TwilioVideoInspector, 'emit');
+    jest.spyOn(RoomMonitor, 'emit');
 
-    TwilioVideoInspector.toggleInspector();
-    expect(TwilioVideoInspector.emit).toHaveBeenCalledWith('opened');
+    RoomMonitor.toggleMonitor();
+    expect(RoomMonitor.emit).toHaveBeenCalledWith('opened');
 
-    TwilioVideoInspector.toggleInspector();
-    expect(TwilioVideoInspector.emit).toHaveBeenCalledWith('closed');
+    RoomMonitor.toggleMonitor();
+    expect(RoomMonitor.emit).toHaveBeenCalledWith('closed');
   });
 
   it('should register a room with the roomRegistry when registerVideoRoom is called', () => {
     jest.spyOn(roomRegistry, 'emit');
 
-    TwilioVideoInspector.registerVideoRoom('mockRoom' as any);
+    RoomMonitor.registerVideoRoom('mockRoom' as any);
     expect(roomRegistry.emit).toHaveBeenCalledWith('roomRegistered', 'mockRoom');
     expect(roomRegistry.room).toBe('mockRoom');
   });
