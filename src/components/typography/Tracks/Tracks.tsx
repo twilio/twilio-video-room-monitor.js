4bbc307cdab6331a2cbc315ef44b830e374cd5fe
Vivian Bai <vbai@twilio.com>
2021-07-14 17:43:31 -0400

    include checks for different LocalTrack types

--- src/components/typography/Tracks/Tracks.tsx
+++ src/components/typography/Tracks/Tracks.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Arr } from '../../../types';
+import { LocalAudioTrack, LocalDataTrack, LocalVideoTrack, Room } from 'twilio-video';
 import { Accordion } from '../Accordion/Accordion';
 import Datum from '../Datum/Datum';
 import MediaStreamTracks from '../MediaStreamTracks/MediaStreamTracks';
 import StatsContainer from '../StatsContainer/StatsContainer';
 
-function Tracks({ tracks }: { tracks: Arr | null | undefined }) {
+function Tracks({ tracks }: { tracks: Room['_options']['tracks'] | null }) {
   if (typeof tracks === 'undefined' || tracks === null) {
     return <Datum label="Tracks" value={tracks} />;
   }
@@ -14,14 +14,15 @@ function Tracks({ tracks }: { tracks: Arr | null | undefined }) {
       {tracks.map((track, i) => {
         if (track instanceof MediaStreamTrack) {
           return (
-            <StatsContainer>
+            <StatsContainer key={i}>
               <MediaStreamTracks track={track} />
             </StatsContainer>
           );
-        } else {
-          const { kind, name, id, mediaStreamTrack }: any = track;
+        }
+        if (track instanceof LocalAudioTrack || track instanceof LocalVideoTrack) {
+          const { kind, name, id, mediaStreamTrack } = track;
           return (
-            <StatsContainer>
+            <StatsContainer key={i}>
               <Datum label="Kind" value={kind} />
               <Datum label="Name" value={name} />
               <Datum label="ID" value={id} />
@@ -29,6 +30,20 @@ function Tracks({ tracks }: { tracks: Arr | null | undefined }) {
             </StatsContainer>
           );
         }
+        if (track instanceof LocalDataTrack) {
+          const { kind, id, maxPacketLifeTime, maxRetransmits, ordered, reliable } = track;
+          return (
+            <StatsContainer key={i}>
+              <Datum label="Kind" value={kind} />
+              <Datum label="ID" value={id} />
+              <Datum label="maxPacketLifeTime" value={maxPacketLifeTime} />
+              <Datum label="maxRetransmits" value={maxRetransmits} />
+              <Datum label="Ordered" value={ordered} />
+              <Datum label="Reliable" value={reliable} />
+            </StatsContainer>
+          );
+        }
+        return null;
       })}
     </Accordion>
   );
