e0472b8af66f8a7edd8b0aff819326209f812ff6
Vivian Bai <vbai@twilio.com>
2021-07-23 15:55:40 -0400

    fix import statements for the RoomInfo file

--- src/components/RoomInfo/RoomInfo.tsx
+++ src/components/RoomInfo/RoomInfo.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Datum from '../typography/Datum/Datum';
-import Headline from '../typography/Headline/Headline';
+import Datum from '../typography/shared/Datum/Datum';
+import Headline from '../typography/shared/Headline/Headline';
 import useDominantSpeaker from '../../hooks/useDominantSpeaker/useDominantSpeaker';
 import useIsRecording from '../../hooks/useIsRecording/useIsRecording';
 import useRoom from '../../hooks/useRoom/useRoom';
 import useRoomState from '../../hooks/useRoomState/useRoomState';
 import useStats from '../../hooks/useStats/useStats';
-import { Accordion } from '../typography/Accordion/Accordion';
-import NestedObject from '../typography/NestedObject/NestedObject';
-import ArrayData from '../typography/ArrayData/ArrayData';
-import ConnectionOptionTracks from '../typography/ConnectionOptionTracks/ConnectionOptionTracks';
+import { Accordion } from '../typography/shared/Accordion/Accordion';
+import NestedObject from '../typography/shared/NestedObject/NestedObject';
+import ArrayData from '../typography/shared/ArrayData/ArrayData';
+import ConnectionOptionTracks from '../typography/trackInformation/ConnectionOptionTracks/ConnectionOptionTracks';
 
 export default function RoomInfo() {
   const room = useRoom();
