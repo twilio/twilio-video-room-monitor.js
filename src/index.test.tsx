1f34a8c06148d5d2edd0773630dd632f69eecba0
Tim Mendoza <tmendoza@twilio.com>
2021-07-30 12:45:35 -0600

    Attach VideoRoomMonitor when parcel target is browser

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,74 +1,98 @@
 import React from 'react';
 import { roomRegistry } from './components/RoomProvider/RoomProvider';
-import RoomMonitor from './index';
+import { VideoRoomMonitor } from './index';
 
 jest.mock('./App', () => () => <div>Test</div>);
 
 describe('the Twilio Video Room Monitor API', () => {
-  afterEach(() => RoomMonitor.closeMonitor());
+  afterEach(() => VideoRoomMonitor.closeMonitor());
 
   it('should render the app to the document when opened, and remove it when closed', () => {
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
 
-    RoomMonitor.openMonitor();
+    VideoRoomMonitor.openMonitor();
     expect(document.querySelector('div')?.textContent).toEqual('Test');
 
-    RoomMonitor.closeMonitor();
+    VideoRoomMonitor.closeMonitor();
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
   });
 
   it('should not render the app when it is already open', () => {
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
 
-    RoomMonitor.openMonitor();
-    RoomMonitor.openMonitor();
+    VideoRoomMonitor.openMonitor();
+    VideoRoomMonitor.openMonitor();
 
-    expect(document.querySelectorAll('#RoomMonitorContainer').length).toEqual(1);
+    expect(document.querySelectorAll('#TwilioVideoRoomMonitorContainer').length).toEqual(1);
   });
 
   it('should not close the app when it is already closed', () => {
     expect(document.querySelector('div')?.textContent).toEqual(undefined);
 
-    RoomMonitor.closeMonitor();
+    VideoRoomMonitor.closeMonitor();
 
-    expect(document.querySelectorAll('#RoomMonitorContainer').length).toEqual(0);
+    expect(document.querySelectorAll('#TwilioVideoRoomMonitorContainer').length).toEqual(0);
   });
 
   it('should emit events when opened and closed', () => {
-    jest.spyOn(RoomMonitor, 'emit');
+    jest.spyOn(VideoRoomMonitor, 'emit');
 
-    RoomMonitor.openMonitor();
-    expect(RoomMonitor.emit).toHaveBeenCalledWith('opened');
+    VideoRoomMonitor.openMonitor();
+    expect(VideoRoomMonitor.emit).toHaveBeenCalledWith('opened');
 
-    RoomMonitor.closeMonitor();
-    expect(RoomMonitor.emit).toHaveBeenCalledWith('closed');
+    VideoRoomMonitor.closeMonitor();
+    expect(VideoRoomMonitor.emit).toHaveBeenCalledWith('closed');
   });
 
   it('should correctly set isOpen when the monitor is opened or closed', () => {
-    jest.spyOn(RoomMonitor, 'emit');
+    jest.spyOn(VideoRoomMonitor, 'emit');
 
-    RoomMonitor.openMonitor();
-    expect(RoomMonitor.isOpen).toBe(true);
+    VideoRoomMonitor.openMonitor();
+    expect(VideoRoomMonitor.isOpen).toBe(true);
 
-    RoomMonitor.closeMonitor();
-    expect(RoomMonitor.isOpen).toBe(false);
+    VideoRoomMonitor.closeMonitor();
+    expect(VideoRoomMonitor.isOpen).toBe(false);
   });
 
   it('should emit events when toggled opened and closed', () => {
-    jest.spyOn(RoomMonitor, 'emit');
+    jest.spyOn(VideoRoomMonitor, 'emit');
 
-    RoomMonitor.toggleMonitor();
-    expect(RoomMonitor.emit).toHaveBeenCalledWith('opened');
+    VideoRoomMonitor.toggleMonitor();
+    expect(VideoRoomMonitor.emit).toHaveBeenCalledWith('opened');
 
-    RoomMonitor.toggleMonitor();
-    expect(RoomMonitor.emit).toHaveBeenCalledWith('closed');
+    VideoRoomMonitor.toggleMonitor();
+    expect(VideoRoomMonitor.emit).toHaveBeenCalledWith('closed');
   });
 
   it('should register a room with the roomRegistry when registerVideoRoom is called', () => {
     jest.spyOn(roomRegistry, 'emit');
 
-    RoomMonitor.registerVideoRoom('mockRoom' as any);
+    VideoRoomMonitor.registerVideoRoom('mockRoom' as any);
     expect(roomRegistry.emit).toHaveBeenCalledWith('roomRegistered', 'mockRoom');
     expect(roomRegistry.room).toBe('mockRoom');
+  });
+
+  it('should attach the VideoRoomMonitor to the window object when the PARCEL_TARGET environment variable is "browser"', () => {
+    // Clears require cache so that we get a fresh import in this test
+    jest.resetModules();
+    process.env.PARCEL_TARGET = 'browser';
+
+    const VideoRoomMonitorImport = require('./index.tsx');
+
+    // @ts-ignore
+    expect(window.Twilio.VideoRoomMonitor).toBe(VideoRoomMonitorImport.VideoRoomMonitor);
+
+    // Resets these values
+    delete process.env.PARCEL_TARGET;
+    // @ts-ignore
+    delete window.Twilio;
+  });
+
+  it('should not attach the VideoRoomMonitor to the window object when the PARCEL_TARGET environment variable is not set', () => {
+    jest.resetModules();
+    require('./index');
+
+    // @ts-ignore
+    expect(window.Twilio).toBe(undefined);
   });
 });
