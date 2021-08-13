a743f2ead84258ca42cf5cc450f5e2452b0bcd69
timmydoza <tmendoza@twilio.com>
2021-08-13 11:35:07 -0600

    Update use publications (#30)
    * Update usePublications hook to periodically update for the local participant
    
    * Fix linting issue
    
    * Update snapshot

--- src/hooks/usePublications/usePublications.test.tsx
+++ src/hooks/usePublications/usePublications.test.tsx
@@ -1,6 +1,9 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import usePublications from './usePublications';
 import EventEmitter from 'events';
+import { UPDATE_INTERVAL } from '../../constants';
+
+jest.useFakeTimers();
 
 describe('the usePublications hook', () => {
   let mockParticipant: any;
@@ -8,44 +11,44 @@ describe('the usePublications hook', () => {
   beforeEach(() => {
     mockParticipant = new EventEmitter();
     mockParticipant.tracks = new Map([
-      [0, 'track1'],
-      [1, 'track2'],
+      [0, { trackSid: 'track1' }],
+      [1, { trackSid: 'track2' }],
     ]);
   });
 
   it('should return an array of mockParticipant.tracks by default', () => {
     const { result } = renderHook(() => usePublications(mockParticipant));
-    expect(result.current).toEqual(['track1', 'track2']);
+    expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }]);
   });
 
   it('should respond to "trackPublished" events', async () => {
     const { result } = renderHook(() => usePublications(mockParticipant));
     act(() => {
-      mockParticipant.emit('trackPublished', 'newMockTrack');
+      mockParticipant.emit('trackPublished', { trackSid: 'newMockTrack' });
     });
-    expect(result.current).toEqual(['track1', 'track2', 'newMockTrack']);
+    expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }, { trackSid: 'newMockTrack' }]);
   });
 
   it('should respond to "trackUnpublished" events', async () => {
     const { result } = renderHook(() => usePublications(mockParticipant));
     act(() => {
-      mockParticipant.emit('trackUnpublished', 'track1');
+      mockParticipant.emit('trackUnpublished', mockParticipant.tracks.get(0));
     });
-    expect(result.current).toEqual(['track2']);
+    expect(result.current).toEqual([{ trackSid: 'track2' }]);
   });
 
   it('should return a new set of tracks if the participant changes', () => {
     const { result, rerender } = renderHook(({ participant }) => usePublications(participant), {
       initialProps: { participant: mockParticipant },
     });
-    expect(result.current).toEqual(['track1', 'track2']);
+    expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }]);
     mockParticipant = new EventEmitter();
     mockParticipant.tracks = new Map([
-      [0, 'track3'],
-      [1, 'track4'],
+      [0, { trackSid: 'track3' }],
+      [1, { trackSid: 'track4' }],
     ]);
     rerender({ participant: mockParticipant });
-    expect(result.current).toEqual(['track3', 'track4']);
+    expect(result.current).toEqual([{ trackSid: 'track3' }, { trackSid: 'track4' }]);
   });
 
   it('should clean up listeners on unmount', () => {
@@ -53,5 +56,41 @@ describe('the usePublications hook', () => {
     unmount();
     expect(mockParticipant.listenerCount('trackPublished')).toBe(0);
     expect(mockParticipant.listenerCount('trackUnpublished')).toBe(0);
+  });
+
+  describe('when isLocal is true', () => {
+    it('should periodically update the conversations array when it changes', async () => {
+      const { result } = renderHook(() => usePublications(mockParticipant, true));
+      expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }]);
+      mockParticipant.tracks.set(2, { trackSid: 'track3' });
+      act(() => {
+        jest.runTimersToTime(UPDATE_INTERVAL);
+      });
+      expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }, { trackSid: 'track3' }]);
+    });
+
+    it('should not periodically update the conversations array when it changes, but trackSids stay the same', async () => {
+      const { result } = renderHook(() => usePublications(mockParticipant, true));
+      expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }]);
+      mockParticipant.tracks = new Map([
+        [0, { trackSid: 'track2' }],
+        [1, { trackSid: 'track1' }],
+      ]);
+      act(() => {
+        jest.runTimersToTime(UPDATE_INTERVAL);
+      });
+      expect(result.current).toEqual([{ trackSid: 'track1' }, { trackSid: 'track2' }]);
+    });
+
+    it('should clear the setInterval ID when the participant changes', async () => {
+      jest.spyOn(window, 'clearInterval');
+      const { rerender } = renderHook(({ participant, isLocal }) => usePublications(participant, isLocal), {
+        initialProps: { participant: mockParticipant, isLocal: true },
+      });
+      mockParticipant = new EventEmitter();
+      mockParticipant.tracks = new Map();
+      rerender({ participant: mockParticipant, isLocal: true });
+      expect(window.clearInterval).toHaveBeenCalledWith(expect.any(Number));
+    });
   });
 });
--- src/hooks/usePublications/usePublications.ts
+++ src/hooks/usePublications/usePublications.ts
@@ -1,10 +1,45 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { LocalTrackPublication, Participant, RemoteTrackPublication } from 'twilio-video';
+import { UPDATE_INTERVAL } from '../../constants';
 
 type TrackPublication = LocalTrackPublication | RemoteTrackPublication;
 
-export default function usePublications(participant: Participant) {
+// Returns true when the two publication arrays contain all the same sids, in any order.
+// This helps to prevent any updates when only the order of publications has chagned.
+function arePublicationArraysEqual(arr1: TrackPublication[], arr2: TrackPublication[]) {
+  const sidString1 = arr1
+    .map((p) => p.trackSid)
+    .sort()
+    .join('');
+  const sidString2 = arr2
+    .map((p) => p.trackSid)
+    .sort()
+    .join('');
+  return sidString1 === sidString2;
+}
+
+export default function usePublications(participant: Participant, isLocal?: boolean) {
   const [publications, setPublications] = useState<TrackPublication[]>([]);
+  const intervalIdRef = useRef<number>();
+
+  useEffect(() => {
+    // Local participants do not emit the 'trackUnpublished' event when a local track is unpublished.
+    // Here we periodically check to see if the publications have changed, and we update the state when
+    // they have changed.
+    if (isLocal) {
+      intervalIdRef.current = window.setInterval(() => {
+        const newPublications = Array.from(participant.tracks.values()) as TrackPublication[];
+        setPublications((oldPublications) =>
+          // Returning the current state here does not trigger a re-render.
+          arePublicationArraysEqual(oldPublications, newPublications) ? oldPublications : newPublications
+        );
+      }, UPDATE_INTERVAL);
+
+      return () => {
+        window.clearInterval(intervalIdRef.current);
+      };
+    }
+  }, [participant, isLocal]);
 
   useEffect(() => {
     // Reset the publications when the 'participant' variable changes.
