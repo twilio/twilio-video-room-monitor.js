cf6ff0c20fae9f4f48de96249115e4d1aea1ff82
Tim Mendoza <tmendoza@twilio.com>
2022-08-11 00:35:16 -0600

    Update MediaStreamTrackInfo component

--- src/components/typography/trackInformation/MediaStreamTrackInfo/MediaStreamTrackInfo.tsx
+++ src/components/typography/trackInformation/MediaStreamTrackInfo/MediaStreamTrackInfo.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import useMediaStreamTrackProperties from '../../../../hooks/useMediaStreamTrackProperties/useMediaStreamTrackProperties';
+import Datum from '../../common/Datum/Datum';
 import NestedObject from '../../common/NestedObject/NestedObject';
 
-function MediaStreamTrackInfo({ track }: { track: MediaStreamTrack | undefined }) {
+function MediaStreamTrackInfo({ track }: { track: MediaStreamTrack | null }) {
   const mediaStreamTrackProperties = useMediaStreamTrackProperties(track);
+
+  if (track === null) {
+    return <Datum label="Media Stream Track" value="null" />;
+  }
+
   return <NestedObject label="Media Stream Track" obj={mediaStreamTrackProperties} />;
 }
 
